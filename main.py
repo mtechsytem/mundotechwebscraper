ee39d4aed87d277cc5388cf4ccf5ed948e09f54a
mtechsytem <saimonfreire@gmail.com>
2025-06-19 01:13:24 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 import requests
 from bs4 import BeautifulSoup
 
@@ -12,6 +13,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 def obter_cotacao():
     try:
